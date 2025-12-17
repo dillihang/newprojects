564544c1d3512ff6b69358bc499cfb61036b5826
Dilli Hang Limbu <dillihang_limbu@hotmail.com>
2025-12-17 23:37:48 +0000

    Add Docker CI/CD workflow

--- tests/test_github_utils.py
+++ tests/test_github_utils.py
@@ -1,23 +1,22 @@
 import sys
 import os
 import tempfile
-import base64
+import base64 
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Devopsandcloudautomation', 'devopsEx15'))
+import pushfiletogitviaapi
 
-from pushfiletogitviaapi import get_file_content
 
 def test_get_file_content_func():
     with tempfile.NamedTemporaryFile(mode="w", suffix=".txt", delete=False) as f:
         f.write("test content. this is just for test")
         temp_path = f.name
 
-    result = get_file_content(temp_path)
+    result = pushfiletogitviaapi.get_file_content(temp_path)
 
-    expected = base64.b64encode(f"test content. this is just for test").decode()
+    expected = base64.b64encode(b"test content. this is just for test").decode()
 
     assert result == expected
-
 
 if __name__=="__main__":
 
